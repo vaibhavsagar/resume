58235527806e13864ed01b57f0c7503f7163a689
vaibhavsagar <vaibhavsagar@gmail.com>
2015-06-18 07:18:50 +1000

    Hardcode pandoc version for now.

--- .ci/prepare.sh
+++ .ci/prepare.sh
@@ -1,5 +1,7 @@
 # Install jq to filter Github release data for Pandoc.
-sudo apt-get -y install jq
+# sudo apt-get -y install jq
 # Get the latest .deb released.
-wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
+# wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
+# Get a release with a .deb for now
+wget https://github.com/jgm/pandoc/releases/download/1.14/pandoc-1.14-1-amd64.deb -O pandoc.deb
 sudo dpkg -i pandoc.deb
