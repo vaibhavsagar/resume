893cce4674f126078828aa6241f0fccd62a3b2ad
Vaibhav Sagar <vaibhavsagar@gmail.com>
2015-11-21 02:04:48 +1100

    Go back to using latest pandoc.

--- .ci/prepare.sh
+++ .ci/prepare.sh
@@ -1,5 +1,5 @@
 # Install jq to filter Github release data for Pandoc.
 sudo apt-get -y install jq
 # Get the latest .deb released.
-wget 'https://github.com/jgm/pandoc/releases/download/1.15.1/pandoc-1.15.1-1-amd64.deb' -O pandoc.deb
+wget `curl https://api.github.com/repos/jgm/pandoc/releases/latest | jq -r '.assets[] | .browser_download_url | select(endswith("deb"))'` -O pandoc.deb
 sudo dpkg -i pandoc.deb
