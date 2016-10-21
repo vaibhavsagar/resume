155587d149e37e08e4903eeb9eceb4b641f42ea5
Vaibhav Sagar <vaibhavsagar@gmail.com>
2016-10-21 10:52:01 -0400

    Install xzdec without sudo.

--- .ci/build_pdf.sh
+++ .ci/build_pdf.sh
@@ -4,7 +4,6 @@ tar xf install-tl-unx.tar.gz
 # Install LaTeX
 sudo install-tl-*/install-tl -profile .ci/texlive.profile
 export PATH=/usr/local/texlive/2016/bin/x86_64-linux:$PATH
-sudo apt-get -y install xzdec
 sudo env PATH="$PATH" tlmgr init-usertree
 sudo env PATH="$PATH" tlmgr install booktabs preprint lm ec titling
 make pdf
