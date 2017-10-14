0ad83264e4e956949686f0c31af0035ce7362f22
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-14 19:10:41 +0800

    Move nix stuff to shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  inherit (nixpkgs) pkgs;
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-basic booktabs ec lm preprint titling;
+  };
+  env = nixpkgs.runCommand "resume-env" {
+    buildInputs = [
+      pkgs.gnumake
+      nixpkgs.haskellPackages.pandoc
+    ];
+    src = ./.;
+  } "";
+in {
+  pdf    = nixpkgs.lib.overrideDerivation env (oldAttrs: {
+    buildInputs = oldAttrs.buildInputs ++ [tex];
+  });
+  travis = env;
+}
