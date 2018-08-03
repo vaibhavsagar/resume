84502f24870313b1a6dd3133c3729f7dbaffc79e
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-08-03 21:58:46 +0800

    default.nix: define src once and inherit

--- default.nix
+++ default.nix
@@ -4,24 +4,25 @@ let
   tex = pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-basic booktabs ec lm preprint titling;
   };
+  src = pkgs.lib.cleanSource ./.;
 in rec {
   html   = pkgs.runCommand "html" {
-    buildInputs = [ nixpkgs.haskellPackages.pandoc ];
-    src = pkgs.lib.cleanSource ./.;
+    inherit src;
+    buildInputs = [ pkgs.haskellPackages.pandoc ];
   } ''
     pandoc $src/resume.md -s -H $src/templates/header.css -o $out
   '';
   pdf    = pkgs.runCommand "pdf" {
-    buildInputs = [ nixpkgs.coreutils nixpkgs.haskellPackages.pandoc tex ];
-    src = pkgs.lib.cleanSource ./.;
+    inherit src;
+    buildInputs = [ pkgs.coreutils pkgs.haskellPackages.pandoc tex ];
   } ''
     workdir=$(mktemp -d)
     pandoc $src/resume.md -H $src/templates/header.tex -o $workdir/Vaibhav_Sagar_resume.pdf
     mv $workdir/Vaibhav_Sagar_resume.pdf $out
   '';
   readme = pkgs.runCommand "html" {
-    buildInputs = [ nixpkgs.haskellPackages.pandoc ];
-    src = pkgs.lib.cleanSource ./.;
+    inherit src;
+    buildInputs = [ pkgs.haskellPackages.pandoc ];
   } ''
     pandoc $src/resume.md -t gfm -o $out
   '';
