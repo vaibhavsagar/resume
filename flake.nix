e70d09854cb6bbbd0653f2b3db5decf418b3fedf
Vaibhav Sagar <vaibhavsagar@gmail.com>
2026-03-02 01:45:45 +1100

    flake.nix: update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
